7a91f7ed5711d8688cd9e29b43fe6a208170c732
ludeeus <joasoe@gmail.com>
2019-04-06 16:25:39 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="pyhaversion",
-    version="2.1.0",
+    version="2.1.1",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="",
