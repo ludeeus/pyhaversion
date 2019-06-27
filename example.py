0db8a760f658f94e055575cfcfc2ecc91089a118
ludeeus <joasoe@gmail.com>
2019-06-27 08:33:34 +0200

    update example

--- example.py
+++ example.py
@@ -7,8 +7,8 @@ from pyhaversion import Version
 async def test():
     """Example usage of pyhaversion."""
     async with aiohttp.ClientSession() as session:
-        data = Version(LOOP, branch="beta", session=session, image="default")
-        await data.get_hassio_version()
+        data = Version(LOOP, branch="beta", session=session, image="qemux86-64")
+        await data.get_docker_version()
 
         print("Version:", data.version)
         print("Attributes:", data.version_data)
