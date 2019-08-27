5cf513015d6cc599b96ec1245026ace495e84066
Fabian Affolter <mail@fabian-affolter.ch>
2019-08-27 07:40:38 +0200

    Update variable name (#2)

--- example.py
+++ example.py
@@ -1,18 +1,19 @@
 """Example usage of pyhaversion."""
 import asyncio
 import aiohttp
+
 from pyhaversion import DockerVersion
 
 
 async def test():
     """Example usage of pyhaversion."""
     async with aiohttp.ClientSession() as session:
-        data = DockerVersion(LOOP, branch="beta", session=session, image="qemux86-64")
+        data = DockerVersion(loop, branch="beta", session=session, image="qemux86-64")
         await data.get_version()
 
         print("Version:", data.version)
         print("Attributes:", data.version_data)
 
 
-LOOP = asyncio.get_event_loop()
-LOOP.run_until_complete(test())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test())
