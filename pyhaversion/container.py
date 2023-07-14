fed11dc25eea552eab39d8099e7b934c07ef5c13
Joakim Sørensen <joasoe@gmail.com>
2023-07-14 09:59:52 +0200

    Remove image handling from container source (#126)

--- pyhaversion/container.py
+++ pyhaversion/container.py
@@ -7,25 +7,22 @@ from aiohttp.client import ClientTimeout
 from awesomeversion import AwesomeVersion
 
 from .base import HaVersionBase
-from .consts import DEFAULT_HEADERS, DEFAULT_IMAGE, HaVersionChannel
+from .consts import DEFAULT_HEADERS, HaVersionChannel
 from .exceptions import HaVersionFetchException
 
-URL = "https://registry.hub.docker.com/v2/repositories/homeassistant/{image}/tags"
+URL = (
+    "https://registry.hub.docker.com/v2/repositories/homeassistant/home-assistant/tags"
+)
 
 
 @dataclass
 class HaVersionContainer(HaVersionBase):
     """Handle versions for the Container source."""
 
-    def validate_input(self) -> None:
-        """Raise HaVersionInputException if expected input are missing."""
-        if self.image is None or self.image == DEFAULT_IMAGE:
-            self.image = "home-assistant"
-
     async def fetch(self, **kwargs):
         """Logic to fetch new version data."""
         request = await self.session.get(
-            url=kwargs.get("url", URL.format(image=self.image)),
+            url=kwargs.get("url", URL),
             headers=DEFAULT_HEADERS,
             timeout=ClientTimeout(total=self.timeout),
         )
