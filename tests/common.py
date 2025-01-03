f82a31fecf229ca25b000f88ed268dd01f785d0e
ludeeus <joasoe@gmail.com>
2025-01-03 17:31:45 +0000

    adjust

--- tests/common.py
+++ tests/common.py
@@ -10,6 +10,5 @@ def fixture(filename: str, asjson: bool = True) -> str | dict:
     """Load a fixture."""
     path = Path(Path(__file__).parent, "fixtures", f"{filename}.json")
     with path.open(encoding="utf-8") as fptr:
-        if asjson:
-            return json.loads(fptr.read())
-        return fptr.read()
+        content = fptr.read()
+        return json.loads(content) if asjson else content
